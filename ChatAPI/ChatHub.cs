f307ad308a39504170b4e40a512bf45f463b28be
Shivam Singh <shivamss17.98@gmail.com>
2025-01-29 20:42:44 +0000

    Backend for Chat app

---
+++ ChatAPI/ChatHub.cs
@@ -0,0 +1,24 @@
+﻿using ChatAPI.Models;
+using Microsoft.AspNetCore.SignalR;
+
+namespace ChatAPI
+{
+    public class ChatHub : Hub
+    {
+        public async Task JoinChat(UserConnection connection)
+        {
+            await Clients.All.SendAsync("ReceiveMessage","admin",$"{connection.UserName} has joined");
+        }
+
+        public async Task JoinSpecificChatRoom(UserConnection connection)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, connection.ChatRoom);
+            await Clients.Group(connection.ChatRoom).SendAsync("ReceiveMessage", "admin", $"{connection.UserName} has joined");
+        }
+
+        public async void SendMessage(string name, string message)
+        {
+            Clients.All.SendAsync("ReceiveMessage", name, message);
+        }
+    }
+}
---
+++ ChatAPI/Models/UserConnection.cs
@@ -0,0 +1,14 @@
+﻿namespace ChatAPI.Models
+{
+    public class UserConnection
+    {
+        public string UserName { get; set; }
+        public string ChatRoom { get; set; }
+
+        public UserConnection()
+        {
+            UserName = string.Empty;
+            ChatRoom = string.Empty;
+        }
+    }
+}
